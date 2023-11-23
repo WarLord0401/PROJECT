e7a461a21e66361da005f662492cacc92a3dde0d
Kanishk <kanishkteotia5077@gmail.com>
2023-11-24 02:02:37 +0530

    shd

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import Home from './pages/Home';
-import Starred from './pages/Starred';
-import Show from './pages/Show';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import img from './attr/hollywood.jpeg';
 import MainLayout from './components/MainLayout';
+import Home from './pages/Home';
+import Show from './pages/Show';
+import Starred from './pages/Starred';
 import { GlobalTheme } from './theme';
-
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalTheme>
+        <img
+          src={img}
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            opacity: '40%',
+            zIndex: -10,
+          }}
+        />
         <HashRouter>
           <Routes>
             <Route element={<MainLayout />}>
